cb066dceee4f3d1eb2ed74169b6227105613c796
Caio Tomaz <102072922+caiomaz@users.noreply.github.com>
2025-02-11 10:23:47 -0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 #==>
 
-SECRET_KEY = 'SILASQUEIROZaimeuDEUSeuCOMMITEIsegred0s!!!"
+SECRET_KEY = "SILASQUEIROZaimeuDEUSeuCOMMITEIsegred0s!!!"
 
 DEBUG = True
 
